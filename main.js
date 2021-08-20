e0faeab982445001b5dec23779cf4a841f56d05e
shmuel94 <shmuelmoche@gmail.com>
2021-08-20 11:27:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// //1
+// console.log(Math.floor(Math.random() * 100));
+
+// //2
+// console.log(Math.floor(Math.random() * 10000));
+
+// //3
+// console.log(Math.floor(Math.random() * 55));
+
+// //4
+// var userInput = prompt("enter number");
+// function randomByUserInput(someNumber) {
+//   console.log(Math.floor(Math.random() * someNumber));
+// }
+// randomByUserInput(userInput);
+
+// //5
+// var userInput = prompt("enter number from 0 to 9");
+// var random = Math.floor(Math.random() * 10);
+
+// function inputEqualRandom(randomNum,someNumber) {
+// if (randomNum == someNumber) {
+// return true;
+// }
+// console.log(inputEqualRandom(random, userInput));
+
+////6
+// var userInput = prompt("enter number from 0 to 99");
+// var randomNumber = Math.floor(Math.random() * 100);
+
+// function inputEqualRandom(random, userNumber) {
+//   if (random == userNumber) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(inputEqualRandom(randomNumber, userInput));
+
+////7
+// var userName = prompt("enter name");
+// var userInput = prompt("enter number from 0 to 9");
+// var random = Math.floor(Math.random() * 3);
+
+// function winOrLoose(firstName, userNumber, randomNum) {
+//   if (randomNum == userNumber) {
+//     document.write(`${firstName} "you win"`);
+//   }
+//   return false;
+// }
+// console.log(winOrLoose(userName, userInput, random));
